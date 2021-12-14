d6d1af340df75e3583d2f1e8eaffbaaa2ddeb5cf
Manoel Moreira <manoelwp@outlook.com>
2021-12-13 23:58:37 -0300

    Início da conexão com o banco de dados

--- Ecommerce/src/index.js
+++ Ecommerce/src/index.js
@@ -2,19 +2,41 @@ import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import MainRouter from './MainRouter';
+// import database from "./database.js"
 
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-*/
 ReactDOM.render(
   <BrowserRouter>
         <MainRouter />
   </BrowserRouter>, 
   document.getElementById('root')
 );
+
+// //Conexão com o banco de dados
+// const Client = require('pg').Client
+// const cliente = new Client({
+//     user: "byteshop",
+//     password: "byteshop",
+//     host: "159.223.106.29",
+//     port: 5432,
+//     database: "database"
+// })
+
+// async function getDados() {
+//   try {
+//     console.log("Iniciando a conexão")
+//     await cliente.connect()
+//     console.log("Conexão bem sucedida")
+    
+//     const resultado = await cliente.query("select * from mercado.produto")
+//     console.table(resultado.rows)
+//   }
+
+//   catch (ex) {
+//     console.log("Ocorreu erro no getDados" + ex)
+//   } 
+  
+//   finally {
+//     await cliente.end()
+//     console.log("Cliente desconectado")
+//   }
+// }

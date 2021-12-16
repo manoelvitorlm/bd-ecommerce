5c9c7aea80d1064159ab39d79af63d94b6a91ba6
Marcus Gabriel <marcus.melo@dcomp.ufs.br>
2021-12-16 11:28:28 -0300

    add outros nomes para cadastro

--- Ecommerce/src/pages/Cadastro.jsx
+++ Ecommerce/src/pages/Cadastro.jsx
@@ -13,32 +13,33 @@ const Cadastro = () => {
             <main>
                 <form className="formulario container-md">
                     <div className="col-md-6">
-                        <label for="inputEmail4" className="form-label">Email</label>
+                        <label for="inputEmail4" className="form-label">Nome do produto</label>
                         <input type="email" className="form-control" id="inputEmail4"></input>
                     </div>
                     <div className="col-md-6">
-                        <label for="inputPassword4" className="form-label">Password</label>
+                        <label for="inputPassword4" className="form-label">Detalhes do produto</label>
                         <input type="password" className="form-control" id="inputPassword4"></input>
                     </div>
-                    <div className="col-12">
-                        <label for="inputAddress" className="form-label">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"></input>
-                    </div>
-                    <div className="col-12">
-                        <label for="inputAddress2" className="form-label">Address 2</label>
-                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
-                    </div>
-                    <div className="col-md-6">
-                        <label for="inputCity" className="form-label">City</label>
-                        <input type="text" className="form-control" id="inputCity"></input>
-                    </div>
                     <div className="col-md-4">
-                        <label for="inputState" className="form-label">State</label>
+                        <label for="inputState" className="form-label">Categoria</label>
                         <select id="inputState" className="form-select">
-                            <option selected>Choose...</option>
-                            <option>...</option>
+                            <option selected>Tipo</option>
+                            <option>Hardware</option>
+                            <option>Eletrônicos</option>
+
                         </select>
                     </div>
+
+                    <div className="col-12">
+                        <label for="inputAddress2" className="form-label">Preço</label>
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+                    </div>
+
+                    <div className="col-md-6">
+                        <label for="inputCity" className="form-label">Quantidade em estoque</label>
+                        <input type="text" className="form-control" id="inputCity"></input>
+                    </div>
+                    
                     <div className="col-md-2">
                         <label for="inputZip" className="form-label">Zip</label>
                         <input type="text" className="form-control" id="inputZip"></input>
